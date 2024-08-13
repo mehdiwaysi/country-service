4c5db84f4d578ed8c58b3b1cfc75b9d3efc6d9b4
Mehdi <mehdiwaysi@yahoo.com>
2024-08-13 15:25:27 +0330

    main

---
+++ cmd/lambda/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	awslambda "github.com/aws/aws-lambda-go/lambda"
+	"github.com/revotech-group/email-service/http/lambda/handlers"
+	"github.com/revotech-group/go-aws/http/lambda"
+	"github.com/revotech-group/go-aws/http/lambda/middleware"
+	"github.com/revotech-group/go-lib/log"
+)
+
+var router *lambda.Router
+
+func init() {
+	router = lambda.NewRouter("/email", true, middleware.HttpMiddleware)
+
+ 
+	router.Route("GET", "v1/countries/name/:name", handlers.GetByCountryNameHandler)
+	router.Route("GET", "v1/countries/region/:region", handlers.GetByCountryRegionHandler)
+	router.Route("GET", "v1/countries/capital/:capital", handlers.GetByCountryCapitalHandler)
+	router.Route("GET", "v1/countries/lang/:lang", handlers.GetByCountryLangHandler)
+	router.Route("GET", "v1/countries/currency/:currency", handlers.GetByCountryCurrencyHandler)
+	router.Route("GET", "v1/countries/alpha/:alphacode", handlers.GetByCountryAlphaCodeHandler)
+	router.Route("GET", "v1/countries", handlers.GetAllCountriesHandler)
+	 
+}
+
+func main() {
+	log.SetupDefaultLogger(log.DefaultLevel(), true)
+
+	awslambda.Start(router.Handler)
+}
---
+++ http/lambda/handlers/get-by-country-alphacode.go
@@ -0,0 +1,22 @@
+package handlers
+
+import (
+ 
+)
+
+func GetByCountryAlphaCodeHandler(ctx context.Context, e events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	request := new(dto.CreateEmailRequest)
+	err := lambda.UnmarshalRequest(e, true, request, true)
+	if err != nil {
+		return lambda.Error(err)
+	}
+
+	f := factory.NewFactory(request.TenantAlias)
+	srv := service.NewEmailService(f)
+	response, err := srv.CreateEmail(ctx, *request)
+	if err != nil {
+		return lambda.Error(err)
+	}
+
+	return lambda.MarshalResponse(http.StatusOK, nil, response)
+}
